f1b45608dfee0fe33a6d71fa6ccba167160467fc
yokwejuste <71908316+yokwejuste@users.noreply.github.com>
2021-07-16 06:58:06 +0100

    update commit

--- lms/settings.py
+++ lms/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'student',
     'teacher',
     'administrator',
+    'user_visit',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'user_visit.middleware.UserVisitMiddleware',
 ]
 
 ROOT_URLCONF = 'lms.urls'
--- student/urls.py
+++ student/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('detail', views.detail, name='detail'),
     path('contact', views.contact, name='contact'),
     path('courses', views.courses, name='courses'),
-    path('element', views.element),
+    path('element', views.element, name='element'),
     path('about', views.about, name='about'),
     path('blog1', views.about, name='blog1'),
     path('blog2', views.about, name='blog2'),
